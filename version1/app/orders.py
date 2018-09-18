59205bd491b7128fc8607c233989b0cea00f6cf6
unknown <gabworks51@gmail.com>
2018-09-18 17:20:12 +0300

    feature (description) this add a new order to the list if the order exist the user should get a message the order exists [finishes #160588035]

--- version1/app/orders.py
+++ version1/app/orders.py
@@ -5,3 +5,9 @@ class DisplayOrders(Resource):
     def get(self):
         return {"orders":[order.successive() for order in orders]}
 
+    def post(self):
+        data = request.get_json()
+        order = Order(data['name'], data["price"],data['description'])
+        orders.append(order)
+
+        return {"message":"Food order created"}, 201
