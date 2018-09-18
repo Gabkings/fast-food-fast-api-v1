3d4eda6a3575a889b623626e0e04475099f5667f
unknown <gabworks51@gmail.com>
2018-09-18 17:35:18 +0300

    feature (description) This fetches a new order details by id If there is no order the user is prompted a mesage saying no orders available [finishes #160588637]

--- version1/app/orders.py
+++ version1/app/orders.py
@@ -1,6 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource
 from .models import Order, orders
+
+class OrderOpreations(Resource):
+    
+
+    def get(self, id):
+        order = Order().get_by_id(id)
+        
+        if order:
+            return {"order": order.successive()}, 200
+
+        return {"message":"Order not found"}, 404
+
+
+
+
 class DisplayOrders(Resource):
     def get(self):
         return {"orders":[order.successive() for order in orders]}
